e57c88a88aa1ed56faaf4680b503b09d4e92f474
Haolin <3130103807@zju.edu.cn>
2018-03-25 00:24:51 -0400

    chat, photo, login, register, profile
    server end
    add chat, photo,
    update login, register, get profile

--- sys/app.js
+++ sys/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var ejs = require('ejs');
 
-var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -15,9 +14,28 @@ var app = express();
 //var connection = require('./model/db');
 var mysql = require('mysql');
 
-var hashmap = require('hashmap');
-
 var fs = require('fs');
+
+var multer = require('multer');
+
+var user_socket = {};
+
+maxSize = 50 * 1024 * 1024
+// var storage = multer.diskStorage({
+//   destination: './images/',
+//   filename: function (req, file, cb) {
+//     crypto.pseudoRandomBytes(16, function (err, raw) {
+//       if (err) return cb(err)
+//
+//       cb(null, raw.toString('hex') + path.extname(file.originalname))
+//     });
+//   }
+// });
+
+var upload = multer({limits: { fileSize: maxSize}});
+
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit: maxSize }));
+app.use(bodyParser.json({limit: '100mb'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,14 +45,39 @@ app.set('view engine', 'html');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(upload.array());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var routes = require('./routes/index');
 app.use('/', routes);
 app.use('/users', users);
 //app.use(express.static(path.join(__dirname, 'images')));
 app.use('/images', express.static(__dirname+'/images/'))
+
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+app.post('/uploadphoto', upload.single('photo'), function(req, res, next) {
+  console.log('photo upload!');
+
+  query_user_info_body = "select * from User_Info where username = \'" + req.body.username + "\'";
+  connection.query(query_user_info_body, function(err, user_rows, fields) {
+    if (err) {
+      console.log('error in photo upload: query user name!');
+      throw err;
+    }
+
+    var pic_name = './images/' + user_rows[0].user_id.toString() + '.JPG';
+    var fstream = fs.createWriteStream(pic_name);
+    fstream.write(req.file.buffer, function () {
+      res.json({url : pic_name.substr(1)});
+    });
+    fstream.end();
+  });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,12 +86,14 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    console.log(err.stack);
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -63,7 +108,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: err
   });
 });
 
@@ -90,7 +135,7 @@ io.on('connection', function(socket) {
     */
     console.log(DATA);
 
-    var query_user_info_body = "select * from User_Info where username = " + DATA.user_name;
+    var query_user_info_body = "select * from User_Info where username = \'" + DATA.user_name + "\'";
     connection.query(query_user_info_body, function(err, user_info_rows, fields) {
       if (err) {
         console.log("error in get_profile: get user info!");
@@ -120,8 +165,13 @@ io.on('connection', function(socket) {
               throw err;
             }
 
-            socket.emit('DATA', {user_info : user_info_rows[0], task_post : task_post_rows,
-              task_serve : task_serve_rows, skills : skill_rows});
+            var pic_name = './images/' + user_rows[0].user_id.toString() + '.JPG';
+            if (!fs.existsSync(pic_name)) {
+              pic_name = undefined;
+            }
+
+            socket.emit('get_profile_res', {user_info : user_info_rows[0], task_post : task_post_rows,
+              task_serve : task_serve_rows, skills : skill_rows, pic_name : pic_name});
           });
         });
       });
@@ -132,25 +182,38 @@ io.on('connection', function(socket) {
     /*
       login function
       params: DATA {user_name, password}
-      returns: DATA {state, info}
-      return type: state(bool) indicates login state \
+      returns: DATA {state, info, message_content}
+      return type: state(bool) indicates login state
                    info(string) indicates failure reason
+                   message_content(string) contains all message haven't been received, fields corresponding to \
+                                            message_queue tabel in db
     */
     console.log(DATA);
 
 
 
-    var query_login_body = "select * from User_Info where username = " + DATA.user_name;
+    var query_login_body = "select * from User_Info where username = \'" + DATA.user_name + "\'";
     connection.query(query_login_body, function(err, rows, fields) {
       if (err) throw err;
-      console.log("error in login!");
+      console.log("error in login: query login error!");
 
       if (rows.length <= 0) {
-        socket.emit("DATA", {state : false, info : "user name not found"});
+        socket.emit("login_res", {state : false, info : "user name not found"});
       } else if (rows[0].password != DATA.password) {
-        socket.emit("DATA", {state : false, info : "wrong password"});
+        socket.emit("login_res", {state : false, info : "wrong password"});
       } else {
-        socket.emit("DATA", {state : true});
+        user_socket[DATA.user_name] = socket.id;
+        query_message_body = "select * from message_queue where receive_user = " +
+                              DATA.user_name;
+
+        connection.query(query_message_body, function(err, message_rows, fields) {
+          if (err) {
+            console.log("error in login: query message error!");
+            throw err;
+          }
+
+          socket.emit("login_res", {state : true, message_content : message_rows});
+        });
       }
 
     });
@@ -175,7 +238,7 @@ io.on('connection', function(socket) {
       }
 
       if (user_name_rows.length > 0) {
-        socket.emit("DATA", {state : false, info : "user name exists"});
+        socket.emit("register_res", {state : false, info : "user name exists"});
       }
 
       var query_email_body = "select * from User_Info where \
@@ -187,7 +250,7 @@ io.on('connection', function(socket) {
         }
 
         if (email_rows.length > 0) {
-          socket.emit("DATA", {state : false, info : "email exists"});
+          socket.emit("register_res", {state : false, info : "email exists"});
         }
 
         var date = new Date();
@@ -204,18 +267,51 @@ io.on('connection', function(socket) {
     });
   });
 
-  socket.on('upload_photo', function(DATA) {
+  socket.on('send_message', function (DATA) {
     /*
-      upload photo
-      params: DATA {photo}
-      returns: None
-    */
+     *  forward message from one user to another
+     *  params: DATA {send_user, receive_user, content}
+     *  returns: None
+     * */
     console.log(DATA);
-    var fstream = fs.createWriteStream('/images/tmp.JPG');
-    fstream.write(DATA.body);
-    fstream.end();
-  });
+
+    var receiver_socket_id = user_socket[DATA.receive_user];
+
+    if (io.sockets.socket[receiver_socket_id] == undefined) {
+      user_socket[DATA.receive_user] = undefined;
+    }
+
+    if (user_socket[DATA.receive_user] == undefined) {
+      insert_message_body = "insert into message_queue \
+        (send_user, receive_user, content) \
+        VALUES( \'" + DATA.send_user + "\', \'" + DATA.receive_user + "\'" + DATA.conten + "\'";
+
+      connection.query(insert_message_body, function(err, result) {
+        if (err) {
+          console.log('error in send message: insert message error!');
+          throw err;
+        }
+      });
+    } else {
+      var date = new Date();
+      var curdate = ""+date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+      io.sockets.socket(receiver_socket_id).emit("push_message", {content: DATA.content, send_user: DATA.send_user, time: curdate});
+    }
 });
 
+  //socket.on('send_message', function ());
+
+  // socket.on('upload_photo', function(DATA) {
+  //   /*
+  //     upload photo
+  //     params: DATA {photo}
+  //     returns: None
+  //   */
+  //   console.log(DATA);
+  //   var fstream = fs.createWriteStream('/images/tmp.JPG');
+  //   fstream.write(DATA.body);
+  //   fstream.end();
+  // });
+});
 
 module.exports = app;
