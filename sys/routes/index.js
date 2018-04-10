db6a1331c628a122d12f86071a41b065e08fc82f
Haolin <3130103807@zju.edu.cn>
2018-04-09 23:18:05 -0400

    modualize

--- sys/routes/index.js
+++ sys/routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
+var router1 = require('./router1');
+var router2 = require('./router2');
 
-var http = require('http');
-var formidable = require('formidable');
-var path = require('path');
-
-/* GET home page. */
+router.use(router1);
+router.use(router2);
 
 module.exports = router;
